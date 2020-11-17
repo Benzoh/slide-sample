5290de2cc77829b412ffbec1b020d2e42c70e723
Benzoh <benzoh.wada@gmail.com>
2020-11-17 22:17:00 +0900

    add sample code

--- slide.js
+++ slide.js
@@ -1,119 +1,37 @@
 var slider = {
   items: $('.slide'),
-  count: $('.slide').length,
   current: 0,
-  directions: new Array,
-  loading: true,
 
-  init: function() {
-    var self = this;
-
-    this.items.each(function(index, element) {
-      self.directions.push($(element).find('img').data('direction'));
-    });
-
-    this.items.find('img').css('position', 'relative');
-    this.setZIndex();
-  },
-
-  setZIndex: function() {
-    var count = this.items.length;
-    var _count = this.items.length;
-
-    for ( var i = 0; i < _count; i++ ) {
-      $(this.items[i]).css('z-index', count);
-      count--;
-    }
-  },
-
-  /**
-   * @param {string} direction
-   */
-  slide: function(target, direction) {
-    var self = this;
-    var beforePosition = $(target).find('img').css('transform');
-
-    $(target).find('img').fadeIn(200);
+  // z-index: 1
+  // fadein
+  // slide
+  // fadeout
+  // z-index: -1
+  // show
+  action: (target) => {
+    console.log('action >>>');
+    $(target).fadeIn(800).addClass('active');
     
-    setTimeout(function() {
-      console.log('2. slide >>>');
-      console.log({direction});
-      console.log({target});
-      
-      switch (direction) {
-        case 'left-right':
-          $(target).find('img').css('transform', 'translateX(100px)  scale(1.5)');
-          break;
-        case 'right-left':
-          $(target).find('img').css('transform', 'translateX(-100px)  scale(1.5)');
-          break;
-        case 'top-bottom':
-          $(target).find('img').css('transform', 'translateY(100px)  scale(1.5)');
-          break;
-        case 'bottom-top':
-          $(target).find('img').css('transform', 'translateY(-100px)  scale(1.5)');
-          break;
-        default:
-          console.log('direction not match');
-      }
-
-      setTimeout(function() {
-        self.fadeout($(target, direction, beforePosition));
-      }, 3000);
-    }, 1000, target, direction);
-  },
-
-  reset: function($target, direction, beforePosition) {
-    console.log('4. reset >>>');
-    console.log({direction});
-
-    $target.find('img').css('transform', beforePosition);
-  },
-
-  fadeout: function($target, direction, beforePosition) {
-    var self = this;
-    $target.fadeOut(600, function() {
-      self.reset($target, direction, beforePosition);
-    });
-    console.log('3. fadeout >>>');
+    setTimeout(() => {
+      $(target).fadeOut(1000, () => {
+        $(target).removeClass('active');
+      });
+    }, 3000);
   },
 
   run: function() {
-    var self = this;
-    var init = new Promise(function(resolve) {
-      self.init();
-      resolve('done!!');
-    });
+    this.action(this.items[this.current]);
+    this.current++;
 
-    init.then(function(res) {
-      // TODO: ローディング？
-      this.loading = false;
-      // $(self.items).parents('.wrapper').addClass('is_show');
-
-      console.log('0. start >>>');
-      self.slide(self.items[self.current], self.directions[self.current]);
-      self.current++;
-
-      var loop = setInterval(() => {
-        console.log('1. loop >>>');
-        self.slide(self.items[self.current], self.directions[self.current]);
-        self.current++;
-        
-        if (self.current === self.items.length) {
-          self.current = 0
-        }
-      }, 5000);
-      console.log('loading >>>', this.loading);
-    })
-    console.log('loading >>>', this.loading);
+    var loop = setInterval(() => {
+      this.action(this.items[this.current]);
+      this.current++;
+      
+      if (this.current === this.items.length) {
+        this.current = 0;
+      }
+    }, 3000);
   },
 }
 
 slider.run();
-
-// -------------
-// test
-// -------------
-// slider.fadeout($('.wrapper'));
-// slider.slide('left-right');
-// slider.slide('top-bottom');
